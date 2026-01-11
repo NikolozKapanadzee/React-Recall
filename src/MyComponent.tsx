a270740f89c5419ab57981c4644885c60502f2d7
NikolozKapanadzee <nkapanadze369@gmail.com>
2026-01-11 17:52:24 +0400

    feat: onchange recall

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 
 function MyComponent() {
-  const [count, setCount] = useState<number>(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const reset = () => {
-    setCount(0);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
+  const [name, setName] = useState<string>("");
+  const [quantity, setQuantity] = useState<number>(0);
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+  function handleQuantity(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuantity(Number(event.target.value));
+  }
 
   return (
     <>
-      <div className="wrapper">
-        <p>{count}</p>
-        <button onClick={increment}>increment</button>
-        <button onClick={reset}>reset</button>
-        <button onClick={decrement}>decrement</button>
+      <div>
+        <input value={name} onChange={handleNameChange} />
+        <p>Name: {name}</p>
+
+        <input type="number" value={quantity} onChange={handleQuantity} />
+        <p>Quantity: {quantity}</p>
       </div>
     </>
   );
 }
+
 export default MyComponent;
