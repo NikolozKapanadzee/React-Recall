8fb771d3848194e27772f360367044aefa0fbe3b
NikolozKapanadzee <nkapanadze369@gmail.com>
2026-02-06 21:10:37 +0400

    recall: Rendering Content Conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import TapButton from "./components/TapButton";
 import { EXAMPLES } from "../src/data";
+
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState<any | null>(null);
   const handleClick = (selectedButton: any) => {
     setSelectedTopic(selectedButton);
     console.log(selectedButton);
   };
   return (
     <>
-      <TapButton click={() => handleClick("components")} label="Components" />
-      <TapButton click={() => handleClick("jsx")} label="JSX" />
-      <TapButton click={() => handleClick("props")} label="Props" />
-      <TapButton click={() => handleClick("state")} label="State" />
-      <div>
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
+      <main>
+        <TapButton click={() => handleClick("components")} label="Components" />
+        <TapButton click={() => handleClick("jsx")} label="JSX" />
+        <TapButton click={() => handleClick("props")} label="Props" />
+        <TapButton click={() => handleClick("state")} label="State" />
+        <div>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </>
+          )}
+        </div>
+      </main>
     </>
   );
 }
