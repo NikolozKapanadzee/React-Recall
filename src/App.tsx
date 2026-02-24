0e5d7345d1159bde90ff9bc206d277a91dff1536
NikolozKapanadzee <nkapanadze369@gmail.com>
2026-02-24 04:39:46 +0400

    recall: forms,inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import useFetch from "./hooks/useFetch";
-
-type User = {
-  id: number;
-  name: string;
-  email: string;
-};
-
-type Post = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-};
+import { useState } from "react";
 
 function App() {
-  const users = useFetch<User>("https://jsonplaceholder.typicode.com/users");
-  const posts = useFetch<Post>("https://jsonplaceholder.typicode.com/posts");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(enteredEmail);
+    console.log(enteredPassword);
+  };
+  const handleEmailChange = (e: any) => {
+    setEnteredEmail(e.target.value);
+  };
+  const handlePasswordChange = (e: any) => {
+    setEnteredPassword(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+    <form onSubmit={handleSubmit} className="p-4 max-w-sm mx-auto space-y-3">
+      <div className="flex flex-col">
+        <label className="text-sm font-medium mb-1">Email</label>
+        <input
+          onChange={handleEmailChange}
+          value={enteredEmail}
+          type="email"
+          placeholder="email"
+          className="border border-gray-300 rounded px-2 py-1 text-sm"
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <label className="text-sm font-medium mb-1">Password</label>
+        <input
+          onChange={handlePasswordChange}
+          value={enteredPassword}
+          type="password"
+          placeholder="password"
+          className="border border-gray-300 rounded px-2 py-1 text-sm"
+        />
+      </div>
+
+      <div className="flex items-center">
+        <button
+          type="submit"
+          className="w-full bg-gray-200 rounded px-2 py-1 text-sm cursor-pointer"
+        >
+          Login
+        </button>
+        <button
+          type="submit"
+          className="w-full bg-gray-200 rounded px-2 py-1 text-sm cursor-pointer"
+        >
+          Reset
+        </button>
+      </div>
+    </form>
   );
 }
 
